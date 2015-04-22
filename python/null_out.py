662c53761e50743299f4432c0fb5c1e5dc4dde22
gui2one <guillaume.rouault001@orange.fr>
2015-04-22 14:34:28 +0200

    added null_out python shelf tool

---
+++ python/null_out.py
@@ -0,0 +1,29 @@
+import sys
+
+if(hou.selectedNodes().__len__() > 0):
+
+    sel = hou.selectedNodes()[0]
+    #print sel.outputs()
+else:
+    hou.ui.displayMessage("Select a node")
+    # if selection is empty exit the tool
+    sys.exit()
+    
+
+#print sel.children()   
+
+nullNode = sel.parent().createNode("null")
+nullNode.setName("OUT", True)
+nullNode.setColor(hou.Color((1.0,0.0,0.0)))
+
+
+
+for conn in sel.outputConnections():
+    index = conn.inputIndex()
+    outputNode = conn.outputNode()
+    outputNode.setInput(index,nullNode)
+    #print outputNode.name
+  
+nullNode.insertInput(0,sel)    
+
+nullNode.moveToGoodPosition()
