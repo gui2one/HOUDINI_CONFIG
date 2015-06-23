8647ddcd141dfe3131aa4cadfa4a918852d76fba
gui2one <guillaume.rouault001@orange.fr>
2015-06-23 13:45:49 +0200

    added OSM_DATA_convert hda

--- python/OSM_data_convert.py
+++ python/OSM_data_convert.py
@@ -1,20 +1,21 @@
 node = hou.pwd()
 geo = node.geometry()
 
-# Add code to modify contents of geo.
-# Use drop down menu to select examples.
+#########
+#### it needs a "osm_file" ui 'file' parameter
+###############
+filePath = hou.parm('osm_file').eval()
 
 import string
 from array import *
 from lxml import etree
 print "OSM Data Converter"
 
-f = open(hou.expandString("$HIP")+"/osm_data/vern.osm",'r')
+#f = open(hou.expandString("$HIP")+"/osm_data/quartier_fonderie.osm",'r')
+f = open(hou.expandString(filePath),"r")
 
 content = f.read()
 root = etree.XML(content)
-
-#print f
 
 
 f.close()
@@ -38,19 +39,24 @@ for element in root.iter("node"):
     
     
 
-#print(str(inc) + " node(s)found")
+print(str(inc) + " node(s)found")
 
-d = dict(root.attrib)
-#print(sorted(d.items()))
-#print(d.items())
+inc = 0
+for element in root.iter("relation"):
+    for item in element.iter("member"):
+        print item.attrib["ref"]
+    inc+=1    
+    
+    
 
+print(str(inc) + " relation(s)found")
 
-geo.addAttrib(hou.attribType.Global,"myAttrib",0);
-geo.setGlobalAttribValue("myAttrib",20)
+idAttr = geo.addAttrib(hou.attribType.Point,"myAttrib","");
+#geo.setAttribValue("myAttrib",20)
 #geo.setGlobalAttribValue("myAttrib",idArray)
-size = geo.globalAttribs()[1].size()
+#size = geo.globalAttribs()[1].size()
 #print size
-nodeId = geo.globalAttribs()[1].dataType()
+#nodeId = geo.globalAttribs()[1].dataType()
 #print latArray[0]
 
 inc = 0
@@ -58,5 +64,6 @@ for element in idArray:
     point = geo.createPoint()
     
     point.setPosition((lonArray[inc],0.0,-latArray[inc]*1.45))
+    point.setAttribValue(idAttr,str(idArray[inc]))
     inc+=1
     
