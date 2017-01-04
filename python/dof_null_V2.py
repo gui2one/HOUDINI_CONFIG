01b6d3e5a24f9497d68234b8385f177bf8b6054a
gui2one <guillaume.rouault.fx@gmail.com>
2017-01-04 21:54:26 +0100

    Fonderie

--- python/dof_null_V2.py
+++ python/dof_null_V2.py
@@ -19,16 +19,25 @@ if hou.selectedNodes().__len__() == 1:
         nullNode = cam.parent().createNode('null')
         nullNode.setName('dof_null_'  + cam.name())
         nullNode.setPosition( cam.position() - hou.Vector2(0,1))
+        nullNode.parmTuple('dcolor').set((0.8,0,0))
+        nullNode.parm("geoscale").set(0.3)
+        nullNode.parm("displayicon").set(2)
+        nullNode.parm("controltype").set(5)        
+        nullNode.parm("shadedmode").set(1)            
         
         cam.parm('dof_target').set(nullNode.path())
         
         expr = 'cam = hou.node(".")'
         expr +='\ncamPos = cam.worldTransform().extractTranslates()'
         expr +='\ndofNull = hou.node(cam.parm("dof_target").evalAsString())'
-        expr +='\ndofNullPos = dofNull.worldTransform().extractTranslates()'        
-        
+        expr +='\nif dofNull:'
+       
+        expr +='\n\tdofNullPos = dofNull.worldTransform().extractTranslates()'        
+        expr +='\n\treturn camPos.distanceTo(dofNullPos)'    
+        expr += '\nelse:'
+        expr +='\n\treturn 5.0'
         expr +='\n'
-        expr +='\nreturn camPos.distanceTo(dofNullPos)'
+      
         
         cam.parm('focus').setExpression(expr,language=hou.exprLanguage.Python)
 
