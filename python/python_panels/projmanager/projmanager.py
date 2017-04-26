e6fa7548cbd775fd5868aa3fbd5fa20a656fc459
gui2one <guillaume.rouault.fx@gmail.com>
2017-04-26 12:46:37 +0200

    fonderie

---
+++ python/python_panels/projmanager/projmanager.py
@@ -0,0 +1,86 @@
+#!python27
+import hou
+import os
+import sys
+import inspect
+inspect.getfile(os)
+
+sys.path.append("C:/Python27/Lib/site-packages/")
+
+
+
+from PySide2 import QtWidgets
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+#
+#
+class MyCustomWidget(QtWidgets.QWidget):
+    """A custom widget class that contains a label."""
+
+    def __init__(self):
+
+        self.rootPath = ""
+        QtWidgets.QWidget.__init__(self)
+
+        # self.rootPath = hou.getenv("HIP")
+        label1 = QtWidgets.QLabel("Hello World")
+
+        self.combo = QtWidgets.QComboBox()
+        self.combo.addItem("JOB")
+        self.combo.addItem("HIP")
+        self.combo.addItem("choose folder ....")
+
+        self.combo.currentIndexChanged.connect(self.comboChanged)
+
+        reloadBtn = QtWidgets.QPushButton("Reload")
+        reloadBtn.released.connect(self.reloadBtnClicked)
+
+        self.listWidget = QtWidgets.QListWidget()
+
+        layout = QtWidgets.QVBoxLayout()
+
+        layout.addWidget(reloadBtn)
+        layout.addWidget(self.combo)
+        layout.addWidget(self.listWidget)
+
+        layout.setAlignment(Qt.AlignTop)
+        self.setLayout(layout)
+
+        # Apply Houdini styling to the main widget.
+        self.setProperty("houdiniStyle", True)
+
+    def comboChanged(self, index):
+        print "combo changed, index : ", index
+        if index == 0:
+            self.rootPath = hou.getenv("JOB")
+        elif index == 1:
+            self.rootPath = hou.getenv("HIP")
+        elif index == self.combo.count() - 1:
+            chosenPath = hou.ui.selectFile("", "select a folder to copy file into")
+            self.rootPath = chosenPath
+            print '!!!!!!!!!!!!!!', self.rootPath
+        print self.combo.count()
+        self.createList()
+
+    def reloadBtnClicked(self):
+        print "clicked", self.rootPath
+
+    def createList(self):
+        files = os.listdir(self.rootPath)
+
+        for file in files:
+            if not os.path.isdir(os.path.join(proj, file)):
+                if file.endswith('.hiplc') or file.endswith('.hip') or file.endswith('hipnc'):
+                    self.listWidget.addItem(file)
+
+        self.listWidget.doubleClicked.connect(self.openHipFile)
+
+    def openHipFile(self, _file):
+        hou.hipFile.load(os.path.join(self.rootPath, _file.data()))
+
+
+proj = hou.getenv('JOB')
+
+def createInterface():
+    custom = MyCustomWidget()
+    return custom
