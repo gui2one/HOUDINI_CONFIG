c75a2137a1d3f6c26c92e190118bbff6c5f7a762
gui2one <guillaume.rouault001@orange.fr>
2018-08-16 19:22:14 +0200

    pre_render_script update

--- python/pre_render_check_for_existing_file.py
+++ python/pre_render_check_for_existing_file.py
@@ -1,17 +1,22 @@
 import os
 import sys
 root = hou.node(".")
-saveName = root.parm("vm_picture").eval()
-# hou.ui.displayMessage(saveName)
-# print dir(os.path)
-exists = os.path.isfile(saveName)
 
-if exists:
-    res = hou.ui.displayMessage(
-        "File already exists. Overwrite ?", buttons=('yes', 'no'))
-    # hou.ui.displayMessage(str(res))
+start_frame = int(root.parm("f1").eval())
+end_frame = int(root.parm("f2").eval())
+increment_frame = int(root.parm("f3").eval())
 
-    if res == 0:
-        pass
-    elif res == 1:
-        raise ValueError('file exists')
+for i in range(start_frame, end_frame+1):
+    saveName = root.parm("vm_picture").evalAsStringAtFrame(i)
+    # hou.ui.displayMessage(saveName)
+    # print dir(os.path)
+    exists = os.path.isfile(saveName)
+    if exists:
+        res = hou.ui.displayMessage(
+            "File already exists. Overwrite ?", buttons=('yes', 'no'))
+        # hou.ui.displayMessage(str(res))
+    
+        if res == 0:
+            pass
+        elif res == 1:
+            raise ValueError('file exists')
