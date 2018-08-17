445db5d6975001bbd0c371a0ae284ef0fcd850c3
gui2one <guillaume.rouault.fx@gmail.com>
2018-08-17 03:00:00 +0200

    destruction update added python script tools

--- python/toolbar/custom_python_tools/debris_sim.py
+++ python/toolbar/custom_python_tools/debris_sim.py
@@ -1,3 +1,4 @@
+import hou
 import toolutils
 import soptoolutils
 
---
+++ python/toolbar/custom_python_tools/destruction_V2/add_animated_static_attrs.py
@@ -0,0 +1,33 @@
+import hou
+
+if len(hou.selectedNodes()):
+    
+    sel = hou.selectedNodes()[0]
+
+    root = sel
+    attr_names = ("active", "animated", "deforming")
+    attr_values = (0, 1, 0)
+    attr_node = None
+    new_nodes = []
+    for i, attr_name in enumerate(attr_names):
+        attr_node = sel.parent().createNode("attribcreate")
+        new_nodes.append(attr_node)
+        attr_node.setInput(0, root)
+
+        # attr_node.moveToGoodPosition()
+        attr_node.parm("name1").set(attr_name)
+        attr_node.parm("type1").set(1)  # integer type
+        attr_node.parm("value1v1").set(attr_values[i])
+
+        if i > 0:
+            attr_node.setPosition(new_nodes[i-1].position() + hou.Vector2(0, -1))
+        root = attr_node
+
+    null_node = sel.parent().createNode('null')
+    null_node.setName("STATIC_ANIMATED_ATTRS", unique_name = True)
+    null_node.setInput(0, new_nodes[len(new_nodes)-1])
+    null_node.setPosition(new_nodes[len(new_nodes)-1].position() + hou.Vector2(0, -1))
+
+
+else:
+        hou.ui.displayMessage("select a node to branch out of")
